b0c6fa742c0a31172992d35e5b3ef52eae6027e9
Dibwe <dleannykalangu@gmail.com>
2021-09-18 09:16:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#   objective 2
+
+#   1. ask the user to repeat the password
+#   2. check if the first password matches the second one
+#   3. display a message if the password do not match,
+#   4. display an error message if the username is less or equal to two letter long
+#   5. string formating style going forward
+
+
+
+username = input("Please input your username:        \t")
+password = input("Please input your password:        \t")
+passwordConf = input("Please confirm your password:  \t")
+flag = 0
+
+print("\n")
+if len(username) <= 2:
+  flag = flag + 1 
+  print("username length too short")
+
+
+if password != passwordConf :
+  flag += 1 
+  print("Your passwords do not match!")
+
+print("\n")
+
+print(f"We have found {flag} flag(s)")
+
+print("\n")
+
+if not flag :
+  print(f"Hey, { username } your password { len(password) * '*' } is {len(password)} letters long.")
+
---
+++ readme.py
@@ -0,0 +1,29 @@
+#   objective 1
+
+#   1. ask the user to input their username and password
+#   2. display a text  the says " Hey xyz your password is ******** is 8 letters long"
+
+#   objective 2
+
+#   1. ask the user to repeat the password
+#   2. check if the first password matches the first one
+#   3. display a message if the password do not match,
+#   4. display an error message if the username is less or equal to two letter long
+#   5. string formating style going forward
+
+#   objective 3
+#   1.  patern matching, our password should have atleast one caps, number, special charcater
+#   2.  introduce colors
+
+#   objective 4
+
+#   1. fix all the issues including: success message, summary text, and
+#   error message displaying simultaneously( they should not)
+
+#   objective 5
+
+#   1. repeat until the correct password is entered
+
+# objective 6
+
+# 1. break the code into functions ( sepation of concern)
